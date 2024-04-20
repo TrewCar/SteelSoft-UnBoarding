19bfed7199002830abc8ada57ef8c85a5caa83cd
Trew-Car <fasdaser312.new@gmail.com>
2024-04-20 13:12:21 +0300

    Добавьте файлы проекта.

---
+++ SteelSoft-UnBoarding/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SteelSoft_UnBoarding.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
